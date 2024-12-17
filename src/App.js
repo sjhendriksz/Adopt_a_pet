f829998030511dc05d7b69305e602b5bd7e8a175
Stephan Hendriksz <sjhendriksz@gmail.com>
2024-12-17 08:50:36 +0200

    Changed a tags to NavLink and Link

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={ <Root /> } >
     <Route index element={ <HomePage /> } />
     <Route path=':type' element={ <HomePage /> } />
+    <Route path=':type/:id' element={ <PetDetailsPage />} />
   </Route>
 ));
 
