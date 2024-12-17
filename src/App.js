d21fe2f90efcbd0a976662c23e785cac949f68c8
Stephan Hendriksz <sjhendriksz@gmail.com>
2024-12-17 08:16:28 +0200

    first remote push

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Root from './components/root';
+
+// Add react-router-dom imports
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+
+// create router with JSX Route elements
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={ <Root /> } >
+    <Route index element={ <HomePage /> } />
+    <Route path=':type' element={ <HomePage /> } />
+  </Route>
+));
+
+function App() {
+  return (
+    // replace below with a Router Provider
+    <RouterProvider router={appRouter} />
+  );
+}
+
+export default App;
