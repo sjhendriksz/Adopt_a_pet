e36440780cb1635c602b28becc70519683033459
Stephan Hendriksz <sjhendriksz@gmail.com>
2024-12-17 10:56:32 +0200

    Project completed

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ const appRouter = createBrowserRouter(createRoutesFromElements(
     <Route index element={ <HomePage /> } />
     <Route path=':type' element={ <HomePage /> } />
     <Route path=':type/:id' element={ <PetDetailsPage />} />
+    <Route path='search' element={ <SearchPage /> } />
+    <Route path='pet-details-not-found' element={ <PetDetailsNotFound /> } />
   </Route>
 ));
 
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -3,7 +3,7 @@ import { getPetDetails } from '../../api/petfinder';
 import Hero from '../../components/hero';
 
 // Import useParams
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 // Import Navigate
 
 const PetDetailsPage = () => {
@@ -27,43 +27,51 @@ const PetDetailsPage = () => {
     getPetsData();
   }, [id]);
 
-  return (
-    <div>
-      {loading ? (
-        <h3>Loading...</h3>
-      ) : error ? (
-        <div>
-          {/* Redirect to /pet-details-not-found if there was an error! */}
-        </div>
-      ) : (
-        <main>
-          <Hero
-            image={data.photos[1]?.full || 'https://i.imgur.com/aEcJUFK.png'}
-            displayText={`Meet ${data.name}`}
-          />
-          <div className="pet-detail">
-            <div className="pet-image-container">
-              <img
-                className="pet-image"
-                src={
-                  data.photos[0]?.medium || 'https://i.imgur.com/aEcJUFK.png'
-                }
-                alt=""
-              />
-            </div>
-            <div>
-              <h1>{data.name}</h1>
-              <h3>Breed: {data.breeds.primary}</h3>
-              <p>Color: {data.colors.primary || 'Unknown'}</p>
-              <p>Gender: {data.gender}</p>
-              <h3>Description</h3>
-              <p>{data.description}</p>
-            </div>
+  if (error){
+    return(
+      <Navigate to={'/pet-details-not-found'} />
+    );
+  }
+  else{
+    return (
+      <div>
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : error ? (
+          <div>
+            {/* Redirect to /pet-details-not-found if there was an error! */}
           </div>
-        </main>
-      )}
-    </div>
-  );
+        ) : (
+          <main>
+            <Hero
+              image={data.photos[1]?.full || 'https://i.imgur.com/aEcJUFK.png'}
+              displayText={`Meet ${data.name}`}
+            />
+            <div className="pet-detail">
+              <div className="pet-image-container">
+                <img
+                  className="pet-image"
+                  src={
+                    data.photos[0]?.medium || 'https://i.imgur.com/aEcJUFK.png'
+                  }
+                  alt=""
+                />
+              </div>
+              <div>
+                <h1>{data.name}</h1>
+                <h3>Breed: {data.breeds.primary}</h3>
+                <p>Color: {data.colors.primary || 'Unknown'}</p>
+                <p>Gender: {data.gender}</p>
+                <h3>Description</h3>
+                <p>{data.description}</p>
+              </div>
+            </div>
+          </main>
+        )}
+      </div>
+    );
+  }
+  
 };
 
 export default PetDetailsPage;
